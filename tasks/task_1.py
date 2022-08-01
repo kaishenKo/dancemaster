3f01080bf0aa7a5b9583d06485c042024e5a0afe
dscott3d <46595931+dscott3d@users.noreply.github.com>
2022-08-01 15:55:36 +1000

    Added task 1

---
+++ tasks/task_1.py
@@ -0,0 +1,8 @@
+"""
+Task 1: Hello World!
+
+Most tutorials start with a basic "hello world" script, so we'll do the same.
+
+Let's see if we can get an environment working and "print" something using Python.
+"""
+
