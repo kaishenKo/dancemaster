4abb4423ab9e4a04a2fd7893441b994e95e9bc37
dscott3d <46595931+dscott3d@users.noreply.github.com>
2022-08-12 14:19:06 +1000

    New task 3

---
+++ tasks/task_3.py
@@ -0,0 +1,9 @@
+"""
+Task 3: Tim Tams
+
+Daniel likes tim tams ... there's a packet of 11, he eats two at a time until there's only one left.
+
+Please print this out.
+
+Some variable: name, number_of_tim_tams
+"""
